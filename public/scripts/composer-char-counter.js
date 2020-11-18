b6d2c341d5308d12892a3bab091cce69dc114f98
Natalia Martian <natalia.martian@gmail.com>
2020-11-18 00:32:54 +0000

    Changed the date adjust library

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+  $('textarea').on('keyup', event => {
+    const tweet = $(event.target).val();
+    const counter = 140 - tweet.length;
+
+    if(counter >= 0) {
+      $("#counter").text(counter);
+      $("#counter").css('color', '#545149');
+      
+    } else {
+      $("#counter").text(- (tweet.length - 140));
+      $("#counter").css('color', 'crimson');
+    }
+  })
+});
